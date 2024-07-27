dc8014cb5cb4e08afcb23c9955df1c3f2fd7b7c6
bipincodes <bipincodes@outlook.com>
2024-07-27 12:59:19 +0530

    add default route

--- src/app.js
+++ src/app.js
@@ -4,7 +4,17 @@ const PORT = process.env.PORT || 3000;
 
 const root = require('./routes/root');
 
+app.set('view engine', 'pug');
+
 app.use('/', root);
+
+app.use((req, res, next) => {
+  res.status(404).render('not-found');
+});
+
+app.use((err, req, res, next) => {
+  console.log('Error Handler Middleware....');
+});
 
 const startupHandler = (err) => {
   if (err) {
